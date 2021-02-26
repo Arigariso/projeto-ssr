8930849c377074e29b89a020f47f6f5cad171c48
Ariane Gariso <72839315+Arigariso@users.noreply.github.com>
2021-02-26 02:56:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// O servidor esta fazendo a renderização da função App //
+
 ReactDOM.hydrate(
   
     <App />,
